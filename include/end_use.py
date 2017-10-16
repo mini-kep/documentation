49705c238131f977eaf7d7bae9d763d0c292732b
e.pogrebnyak@gmail.com <e.pogrebnyak@gmail.com>
2017-10-16 15:48:44 +0300

    Deployed 8c6e094 with MkDocs version: 0.16.3

--- include/end_use.py
+++ include/end_use.py
@@ -9,3 +9,10 @@ def read_ts(source_url):
 
 er = read_ts('http://mini-kep.herokuapp.com/ru/series/USDRUR_CB/d/2017/')
 brent = read_ts('http://mini-kep.herokuapp.com/oil/series/BRENT/d/2017/')
+
+
+
+# example with partial
+from functools import partial
+read_csv = partial(pd.read_csv, converters={0: pd.to_datetime}, index_col=0)
+read_json = partial(pd.read_json, precise_float=True, orient='split')
